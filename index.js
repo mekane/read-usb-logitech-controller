27926d807f946080cb6935327756438c7022857b
Marty Kane <marty.kane@gmail.com>
2020-11-28 15:33:31 -0600

    Got it to read events / button states from the controller

--- index.js
+++ index.js
@@ -13,7 +13,30 @@ async function main() {
         process.exit(0);
     }
 
-    const interface0 = controller.interfaces[0];
-    console.log('INTERFACE');
-    console.log(interface0);
+    openInterface(controller.interfaces[0]);
+}
+
+
+const logitechInEndpointAddress = 129;
+const closeEndpoints = true;
+
+function openInterface(usbInterface) {
+    usbInterface.claim();
+    //console.log(`Claimed Interface. Driver Active: ${usbInterface.isKernelDriverActive()}`);
+
+    const endpoint = usbInterface.endpoint(logitechInEndpointAddress);
+
+    endpoint.on('data', dataBuf => {
+        console.log(dataBuf);
+    })
+    endpoint.startPoll();
+
+    /*
+    //TODO: call when done
+    endpoint.stopPoll();
+    usbInterface.release(closeEndpoints, err => {
+        if (err)
+            console.log(err)
+    });
+     */
 }
